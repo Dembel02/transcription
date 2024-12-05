993c53da6bacf65be0211524db0049152454e9f2
Dembel02 <IRGilyazov@yandex.ru>
2024-12-05 16:14:27 +0500

    1

--- download_Llama.py
+++ download_Llama.py
@@ -1,10 +1,10 @@
 import requests
 
-# url = "https://huggingface.co/CRD716/ggml-vicuna-1.1-quantized/resolve/main/ggml-vicuna-13B-1.1-q4_0.bin"
-url = "https://huggingface.co/IlyaGusev/saiga_7b_lora_llamacpp/resolve/main/ggml-model-q4_1.bin"
+url = "https://huggingface.co/CRD716/ggml-vicuna-1.1-quantized/resolve/main/ggml-vicuna-13B-1.1-q4_0.bin"
+# url = "https://huggingface.co/IlyaGusev/saiga_7b_lora_llamacpp/resolve/main/ggml-model-q4_1.bin"
 
-# output_path = "ggml-vicuna-13B-1.1-q4_0.bin"
-output_path = "ggml-model-q4_1.bin"
+output_path = "ggml-vicuna-13B-1.1-q4_0.bin"
+# output_path = "ggml-model-q4_1.bin"
 
 response = requests.get(url, stream=True)
 if response.status_code == 200:
